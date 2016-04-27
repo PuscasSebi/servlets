81627cd4908da9faa617b03fdc8a0afff0804f35
SebastianPuscas <Sebastian.Puscas@msg-systems.com>
2016-04-27 14:50:43 +0300

    added setters and getters

--- servlet.learn/src/main/java/servlet/learn/Main.java
+++ servlet.learn/src/main/java/servlet/learn/Main.java
@@ -1,11 +1,18 @@
 package servlet.learn;
 
 public class Main {
-
+	private String Sebastian;
 	public static void main(String[] args) {
 		System.out.println("SEBI E TARE ca racu eeee");
 		System.out.println("SEBI,nu oricine, E TARE");
 
 	}
+	public String getSebastian() {
+		return Sebastian;
+	}
+	public void setSebastian(String sebastian) {
+		Sebastian = sebastian;
+	}
+	
 
 }
