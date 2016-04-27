1ec3b813ff43b5467dff34d4d1ed8fdb791238d1
SebastianPuscas <Sebastian.Puscas@msg-systems.com>
2016-04-27 13:15:19 +0300

    added new text

--- servlet.learn/src/main/java/servlet/learn/Main.java
+++ servlet.learn/src/main/java/servlet/learn/Main.java
@@ -3,7 +3,7 @@ package servlet.learn;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("SEBI E TARE");
+		System.out.println("SEBI E TARE ca racu eeee");
 
 	}
 
