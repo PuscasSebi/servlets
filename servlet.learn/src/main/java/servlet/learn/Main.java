5b3ba40cca48822af53413d1e0e055c32bf10eac
Puscas Sebi <puscas.sebastian@gmail.com>
2016-04-27 14:47:26 +0300

    Update Main.java

--- servlet.learn/src/main/java/servlet/learn/Main.java
+++ servlet.learn/src/main/java/servlet/learn/Main.java
@@ -3,7 +3,7 @@ package servlet.learn;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("SEBI E TARE ca racu eeee");
+		System.out.println("Nu mi place la mare");
 		System.out.println("SEBI,nu oricine, E TARE");
 
 	}
